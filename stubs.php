9bcdecf3c56ca2fcbb404773113129b5a8290e26
Ivan Shalganov <aco.best@gmail.com>
2018-07-10 00:10:42 +0300

    Create stubs.php

---
+++ stubs.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Interface allows to cast the object to boolean value
+ */
+interface Boolable 
+{
+    /**
+     * Cast to boolean value
+     */
+    public function __toBoolean();
+}
